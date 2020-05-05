7e383fa43ca4951eb0464ca5cee5e4d1ec2ec9bc
bennya98 <bennya98@gmail.com>
2020-05-05 16:53:00 +0100

    commented forms.py

--- taskmanager/forms.py
+++ taskmanager/forms.py
@@ -5,9 +5,11 @@ from .models import Task, Project, Journal
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
-        #fields = '__all__'
+
+        # Project field excluded as a task is created within a given project so the field is pre-determined
         exclude = ['project', ]
 
+        # Large text area for task description
         widgets = {
             'Description': forms.Textarea(),
         }
@@ -22,7 +24,14 @@ class ProjectForm(forms.ModelForm):
 class JournalForm(forms.ModelForm):
     class Meta:
         model = Journal
+
+        # other 3 fields are excluded as they are pre-determined variables (Task, Project, Date)
         fields = ['entry', ]
+
+        # Large text area for comment
+        widgets = {
+            'entry': forms.Textarea(),
+        }
 
 
 class DeleteTaskForm(forms.ModelForm):
@@ -30,7 +39,8 @@ class DeleteTaskForm(forms.ModelForm):
         model = Task
         fields = []
 
+
 class DeleteProjectForm(forms.ModelForm):
     class Meta:
-        model = Task
+        model = Project
         fields = []
